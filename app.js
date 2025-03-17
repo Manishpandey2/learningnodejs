0541e3cd768e6b50cb016e8fe513a23d7fc851ed
Manishpande2 <biraalogang@gmail.com>
2025-03-17 07:59:53 +0545

    add EJS views for home, contact, and about pages; configure Express app routing

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 let express = require("express");
 let app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("home");
+});
+
+app.get("/contact", (req, res) => {
+  console.log(req);
+  res.render("contact");
+});
+app.get("/about", (req, res) => {
+  let name = "Manish";
+  res.render("about", { name });
 });
 
 app.listen(3000, () => {
